d9d78f45eb5b191dac9b9fe4344e1a021158f801
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-05-19 21:13:53 -0300

    Project Sword Fantasy - animation suffix fixed

--- sword-fantasy/character/character.gd
+++ sword-fantasy/character/character.gd
@@ -5,14 +5,14 @@ var _animation_suffix: String = "_down"
 @export var _character_animation: AnimationPlayer
 
 func _physics_process(_delta: float) -> void:
-	var basic_move = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var basic_move = Input.get_vector(
+		"move_left", "move_right", "move_up", "move_down")
 	
 	velocity = basic_move * _move_speed
 	move_and_slide()
 	
 	_animation_suffix = _char_suffix()
 	_animation()
-	print(_animation_suffix)
 
 func _char_suffix() -> String:
 	var _horizontal_action: float = Input.get_axis("move_left", "move_right")
@@ -29,11 +29,10 @@ func _char_suffix() -> String:
 	if _vertical_action == +1:
 		return "_down"
 		
-	return "_animation_suffix"
+	return _animation_suffix
 
 func _animation() -> void:
 	if velocity:
 		_character_animation.play("walking" + _animation_suffix)
 		return
-
 	_character_animation.play("look" + _animation_suffix)
